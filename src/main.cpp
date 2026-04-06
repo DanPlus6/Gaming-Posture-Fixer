2af4dc0ea0b33c2fc171b5f84c095d1bd65a4a6f
David Fu <d4nplus6@gmail.com>
2026-04-06 18:01:41 -0400

    add prototype image drawing code

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,8 @@
 #include <cstdio>
 using namespace std;
 
-constexpr UINT  REMINDER_DELAY      = 10;
-constexpr UINT  OVERLAY_DURATION    = 5;
+constexpr UINT  REMINDER_DELAY_S    = 10;
+constexpr UINT  OVERLAY_DURATION_MS = 5000;
 constexpr WCHAR OVERLAY_CLASS[]     = L"PostureOverlay";
 constexpr UINT  TIMER_DISMISS       = 1;
 
@@ -39,9 +39,108 @@ static HBITMAP loadBitmap() {
     return hbm;
 }
 
+/* window proc */
+static LRESULT CALLBACK OverlayProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
+    switch (msg) {
+        case WM_PAINT: {
+            PAINTSTRUCT ps;
+            HDC  hdc  = BeginPaint(hwnd, &ps);
+            auto hbmp = reinterpret_cast<HBITMAP>(GetWindowLongPtr(hwnd, GWLP_USERDATA));
+
+            if (hbmp) {
+                HDC     mdc = CreateCompatibleDC(hdc);
+                HBITMAP old = static_cast<HBITMAP>(SelectObject(mdc, hbmp));
+                BITMAP  bm  = {};
+                GetObject(hbmp, sizeof(bm), &bm);
+
+                RECT r = {};
+                GetClientRect(hwnd, &r);
+                StretchBlt(
+                    hdc, 0, 0, r.right, r.bottom,
+                    mdc, 0, 0, bm.bmWidth, bm.bmHeight, SRCCOPY
+                );
+
+                SelectObject(mdc, old);
+                DeleteDC(mdc);
+            } else {
+                RECT r = {};
+                GetClientRect(hwnd, &r);
+                FillRect(hdc, &r, static_cast<HBRUSH>(GetStockObject(BLACK_BRUSH)));
+                SetTextColor(hdc, RGB(255, 80, 80));
+                SetBkMode(hdc, TRANSPARENT);
+                DrawText(
+                    hdc, reinterpret_cast<LPCSTR>(L"FIX YO DAMN POSTURE GNG"), -1, &r,
+                    DT_CENTER | DT_VCENTER | DT_SINGLELINE
+                );
+            }
+
+            EndPaint(hwnd, &ps);
+            return 0;
+        }
+
+        case WM_TIMER: case WM_LBUTTONDOWN:
+            KillTimer(hwnd, TIMER_DISMISS);
+            DestroyWindow(hwnd);
+            return 0;
+
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+    }
+
+    return DefWindowProc(hwnd, msg, wp, lp);
+}
+
+/* register overlay class once per proc */
+static void ensureClassRegistered() {
+    static bool done = false;
+    if (done) return;
+
+    WNDCLASSEX wc = { sizeof(wc) };
+    wc.lpfnWndProc = OverlayProc;
+    wc.hInstance = GetModuleHandle(nullptr);
+    wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wc.lpszClassName =  reinterpret_cast<LPCSTR>(OVERLAY_CLASS);
+    RegisterClassEx(&wc);
+    done = true;
+}
+
 /* Callback to draw and remove reminder image */
 static void drawAndRemove() {
-    
+    ensureClassRegistered();
+
+    const int sw = GetSystemMetrics(SM_CXSCREEN);
+    const int sh = GetSystemMetrics(SM_CYSCREEN);
+    const int ow = sw;
+    const int oh = sh / 8;
+    const int oy = (sh - oh) / 2;
+
+    HBITMAP hbmp = loadBitmap();
+
+    HWND hwnd = CreateWindowEx(
+        WS_EX_TOPMOST | WS_EX_TOOLWINDOW,
+        reinterpret_cast<LPCSTR>(OVERLAY_CLASS), nullptr,
+        WS_POPUP | WS_VISIBLE,
+        0, oy, ow, oh,
+        nullptr, nullptr, GetModuleHandle(nullptr),
+        nullptr
+    );
+
+    if (!hwnd) {
+        if (hbmp) DeleteObject(hbmp);
+    }
+
+    SetWindowLongPtr(hwnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(hbmp));
+    SetWindowPos(hwnd, HWND_TOPMOST, 0, oy, ow, oh, SWP_SHOWWINDOW);
+    SetTimer(hwnd, TIMER_DISMISS, OVERLAY_DURATION_MS, nullptr);
+
+    MSG msg = {};
+    while (GetMessage(&msg, nullptr, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    if (hbmp) DeleteObject(hbmp);
 }
 
 /* Callback to play reminder audio */
@@ -51,22 +150,20 @@ static void playAudio() {
 
 /* Reminder thread entrypoint */
 static void postureReminder() {
-    int localDelay = REMINDER_DELAY;
-    ULONGLONG runtime = 0;
-    ULONGLONG iteration = 0;
+    int       localDelay = REMINDER_DELAY_S;
+    ULONGLONG runtime    = 0;
+    ULONGLONG iteration  = 0;
+
     while (running) {
         if (localDelay > 0) {
             printf("[+] Delayed 1 second | Seconds left: %d | Total seconds elapsed: %llu\n", localDelay, runtime);
-
             this_thread::sleep_for(chrono::seconds(1));
             localDelay--;
         } else {
             printf("[+] Iteration %llu\n", ++iteration);
-
             playAudio();
             drawAndRemove();
-
-            localDelay = REMINDER_DELAY;
+            localDelay = REMINDER_DELAY_S;
         }
         runtime++;
     }
