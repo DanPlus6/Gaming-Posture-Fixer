8d928dd7ea47a46b73949ceb2d4a70af680aaf5e
David Fu <d4nplus6@gmail.com>
2026-04-06 13:10:50 -0400

    updated debugging for iterations

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ void keybindListener() {
         bool l     = GetAsyncKeyState('L')        & 0x8000;
         
         if (ctrl && shift && l) {
-            printf("[+] ------------------ Keybind pressed, program terminating ------------------\n");
+            printf("[+] ------------------------ Keybind pressed, program terminating ------------------------\n");
             running = false;
             exit(0);
         }
@@ -35,8 +35,7 @@ void postureReminder() {
             this_thread::sleep_for(chrono::seconds(1));
             localDelay--;
         } else {
-            printf("[+] Iteration %llu\n", iteration);
-            iteration++;
+            printf("[+] Iteration %llu\n", ++iteration);
 
             localDelay = delay;
         }
@@ -46,7 +45,7 @@ void postureReminder() {
 
 
 int main() {
-    printf("[+] ------------------ Entrypoint triggered ------------------ \n");
+    printf("[+] --------------------- Entrypoint triggered --------------------- \n");
 
     thread listener(keybindListener);
     thread reminder(postureReminder);
@@ -54,6 +53,6 @@ int main() {
     reminder.join();
     listener.detach();
 
-    throw("[-] ------------------ !!! Main thread terminated unexpectedly !!! ------------------ \n");
+    throw("[-] --------------------- !!! Main thread terminated unexpectedly !!! --------------------- \n");
     return 0;
 }
